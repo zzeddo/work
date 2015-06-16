7ee10246722436da2ad2651488d792e487787683
Park Jae Do <zzeddo@gmail.com>
2015-06-17 06:42:20 +0900

    Update mcmc.R

--- mcmc.R
+++ mcmc.R
@@ -300,3 +300,62 @@ plot(theta1)
 theta2 <- mcmc_samples[,3]
 summary(theta2)
 plot(theta2)
+###########################
+# 3.2 Difference between two rates (R2jags)
+###########################
+# clears workspace:  
+rm(list=ls()) 
+
+#JARS
+library(R2jags)
+
+# 1. Model
+modelString = "
+model {
+# Observed Counts
+theta1 ~ dbeta(1, 1)
+theta2 ~ dbeta(1, 1)
+# Prior on Rates
+k1 ~ dbin(theta1, n1)
+k2 ~ dbin(theta2, n2)
+# Difference Between Rates 
+delta <- theta1-theta2
+}"
+# 2. Data
+mydata = list(k1=5, n1=10, k2=7, n2=10)
+
+# 3. Start Values
+myinits = list(
+  list(theta1 = 0.1, theta2 = 0.9)
+)
+
+# 4. Paramters to be monitored
+parameter = c("delta", "theta1", "theta2")
+
+# The following command calls JAGS with specific options.
+# For a detailed description see the R2jags documentation.
+mcmc_samples <- jags(mydata, inits=myinits, parameter,
+                model.file =textConnection(modelString), n.chains=1, n.iter=10000, 
+                n.burnin=1, n.thin=1, DIC=T)
+
+# Collect posterior samples:
+delta <- mcmc_samples$BUGSoutput$sims.list$delta
+#plot(delta)
+summary(delta)
+# mean of delta:
+mean(delta)
+# median of delta:
+median(delta)
+# mode of delta, estimated from the "density" smoother:
+density(delta)$x[which(density(delta)$y==max(density(delta)$y))]
+# 95% credible interval for delta:
+quantile(delta, c(.025,.975))
+
+# Now let's plot a histogram for delta. 
+# First, some options to make the plot look better:
+par(cex.main = 1.5, mar = c(5, 6, 4, 5) + 0.1, mgp = c(3.5, 1, 0), cex.lab = 1.5,
+    font.lab = 2, cex.axis = 1.3, bty = "n", las=1)
+Nbreaks <- 80
+y       <- hist(delta, Nbreaks, plot=F)
+plot(c(y$breaks, max(y$breaks)), c(0,y$density,0), type="S", lwd=2, lty=1,
+     xlim=c(-1,1), ylim=c(0,4), xlab="Difference in Rates", ylab="Posterior Density") 
